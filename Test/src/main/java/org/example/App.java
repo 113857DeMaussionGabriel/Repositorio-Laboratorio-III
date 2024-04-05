1bd4a66ae6076864aa24bbceed9e19ef1e99c65c
113857_DeMaussionGabriel <113857@tecnicatura.frc.utn.edu.ar>
2024-04-05 16:24:09 -0300

    Clase 2

--- Test/src/main/java/org/example/App.java
+++ Test/src/main/java/org/example/App.java
@@ -4,10 +4,71 @@ package org.example;
  * Hello world!
  *
  */
-public class App 
+public class App
 {
     public static void main( String[] args )
     {
+        int numero = 128;
+        boolean flag = true;
+        String cadena = "Hola";
+        String cadena2 = "Mundo";
+
+
         System.out.println( "Hello World!" );
+
+
+
+        if (cadena.contains("h"))
+        {
+            System.out.println(cadena + " " + cadena2 + "!");
+            System.out.println("Condicional Si");
+        }
+        else
+        {
+            System.out.println("Condicional No");
+        }
+
+
+        if(flag)
+        {
+            System.out.println("Bool True");
+        }
+
+
+        String mayus;
+
+        mayus = cadena.toUpperCase();
+
+        if (mayus.contains("A"))
+        {
+            System.out.println(mayus);
+            System.out.println("Condicional Si en Mayus");
+        }
+
+        String CadenaConcatenada = cadena.concat(" " + cadena2 + " (Concatenada)");
+        System.out.println(CadenaConcatenada);
+
+        char letra = CadenaConcatenada.charAt(2);
+
+        System.out.println("La letra en -segundo- TERCER lugar de la cadena concatenada es: " + letra);
+        System.out.println("(Tener en cuenta que los indices empiezan en 0, no en 1)");
+
+        int i = 0;
+        boolean bCiclo = false;
+
+        System.out.println("\nDeletreo de cadena concatenada:\n");
+
+        while (i < CadenaConcatenada.length() && bCiclo == false)
+        {
+            if("(".equals(CadenaConcatenada.charAt(i)))
+            {
+                bCiclo = true;
+            }
+            System.out.println(CadenaConcatenada.charAt(i));
+            i++;
+        }
+
+
+
     }
 }
